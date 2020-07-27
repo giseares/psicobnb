503ca0d97ff16afa3fed3257c07c620a8be6cdce
Fabricio Butron <fa.butron.rodriguez@gmail.com>
2020-07-27 18:20:52 -0400

    Eliminado la columna de password de users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_212924) do
+ActiveRecord::Schema.define(version: 2020_07_27_221956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 2020_07_27_212924) do
     t.string "available_at"
     t.string "first_name"
     t.string "last_name"
-    t.string "password"
     t.string "address"
     t.string "cel_phone"
     t.string "dni"
