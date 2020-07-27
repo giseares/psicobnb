05d29ed5613a5572ffeeb8517e9ecb418754f0e2
Fabricio Butron <fa.butron.rodriguez@gmail.com>
2020-07-27 19:34:26 -0400

    Aumentando algunas cosas para el devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_action :configure_permitted_parameters, if: :devise_controller?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :appointments, foreign_key: :client_id
   has_many :reservations, foreign_key: :professional_id, class_name: "Appointment"
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_07_27_222130) do
 
   # These are extensions that must be enabled in order to support this database
@@ -50,7 +49,7 @@ ActiveRecord::Schema.define(version: 2020_07_27_222130) do
     t.index ["appointment_id"], name: "index_reviews_on_appointment_id"
   end
 
-  create_table "users", force: :cascade do |t|
+heroku   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
