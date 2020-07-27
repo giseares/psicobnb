2cc2e1edce33b9f8b69a6f38c0b625764a5cc49a
Fabricio Butron <fa.butron.rodriguez@gmail.com>
2020-07-27 17:52:42 -0400

    Aumentando has many

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :appointments, foreign_key: :client_id
+  has_many :appointments, foreign_key: :professional_id
 end
