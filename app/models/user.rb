be256a88a90ba3e2b73b6cee9bca767cc11fdff3
Fabricio Butron <fa.butron.rodriguez@gmail.com>
2020-07-28 10:33:39 -0400

    Terminado

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :appointments, foreign_key: :client_id
   has_many :reservations, foreign_key: :professional_id, class_name: "Appointment"
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/ayuda', to: 'pages#help' as: 'ayuda'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources: users, only: [:show, :index, :create, :new, :edit, :update] do
-    resources: appointments, only: [:index, :show]
-    resources: reviews, only: [:show, :index]
-    get '/profile', to: 'users#profile' as: 'profile' 
+  get '/ayuda', to: 'pages#help', as: 'ayuda'
+  resources :users, only: [:show, :index, :create, :new, :edit, :update] do
+    resources :appointments, only: [:index, :show]
+    resources :reviews, only: [:show, :index]
+    get '/profile', to: 'users#profile', as: 'profile' 
   end
 
-  resources: appointments, only: [:edit, :update, :index, :show] do
-    resources: reviews, only: [:new, :create, :edit, :update]
+  resources :appointments, only: [:edit, :update, :index, :show] do
+    resources :reviews, only: [:new, :create, :edit, :update]
   end
 end
