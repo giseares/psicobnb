ace4f040bc6b2ff6105404a51ed667790921d87c
Gise Ares <gise.ares@gmail.com>
2020-07-28 13:05:16 -0300

    appointment controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/ayuda', to: 'pages#help', as: 'ayuda'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :index, :create, :new, :edit, :update] do
-    resources :appointments, only: [:index, :show]
+    resources :appointments, only: [:index, :show, :create]
     resources :reviews, only: [:show, :index]
     get '/profile', to: 'users#profile', as: 'profile' 
   end
