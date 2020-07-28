5eedb5fbc3e1ff8f1c7e59abadf0e6c5109a069e
Rodrigo Villanueva <r_villanueva18@hotmail.com>
2020-07-28 12:35:44 -0300

    Reviews Controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,49 @@
 class ReviewsController < ApplicationController
+  before_action :set_review, only: %i[show edit create update ]
+
+  def index
+    @reviews = Review.all
+  end
+  
+  def show
+  end
+    
+  def new
+    @appointment = Appointment.find(params[:appointment_id])
+    @review = Review.new
+  end
+
+  def create
+    @appointment = Appointment.find(params[:appointment_id])
+    @review.appointment = @appointment
+    if @review.save
+      redirect_to user_appointments_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:appointment_id])
+    if @review.update(review_params)
+      redirect_to user_appointments_path
+    else
+      render :new
+    end
+  end
+ 
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+ 
+  def review_params
+    params.require(:review).permit(:rating_sharp, :rating_qualityprice, :rating_facilities)
+  end
+
 end
