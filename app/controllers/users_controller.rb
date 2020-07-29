435fdeb2a18c202b73cf757ae5aed43cb6ab1a8b
Fabricio Butron <fa.butron.rodriguez@gmail.com>
2020-07-29 09:55:34 -0400

    Terminado el perfil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,4 +15,25 @@ class UsersController < ApplicationController
     @user = User.find(@profile.user_id)
     authorize @user
   end
+
+  def update_profile
+    @profile = Profile.find_by user_id: params[:user_id]
+    if @profile.update(profile_params)
+      @user = User.find(params[:user_id])
+      authorize @user
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+    # @profile.school = params[:profile][:school]
+    # @profile.license_number = params[:profile][:license_number]
+    # @profile.speciality = params[:profile][:speciality]
+    # @profile.price = params[:profile][:price]
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:school, :license_number, :speciality, :price)
+  end
 end
