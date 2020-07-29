1f7803e8de33ba895432890d1d5f0854bf1e2349
Gise Ares <gise.ares@gmail.com>
2020-07-29 14:03:29 -0300

    modifcaciones de vista

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dni, :address, :cel_phone])
   end
   # La siguiente linea es para incluir Pendit para poder dar autorizacion en las paginas segun los permisos que digamos
-  include Pundit
+  # include Pundit
 
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   # La siguiente linea permite redirigir al usuario cuando entra a una pagina no autorizada
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
