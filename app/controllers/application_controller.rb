6f6976be7604a8041aa0c28c75bd756afbb654c9
julianoviedo <julianoviedo1@gmail.com>
2020-07-31 16:03:01 -0300

    login en show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :store_action
 
   def configure_permitted_parameters
     # Agrega los campos extra
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :photo ])
-
+    
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dni, :address, :cel_phone, :photo])
   end
   # La siguiente linea es para incluir Pendit para poder dar autorizacion en las paginas segun los permisos que digamos
   include Pundit
-
+  
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-
+  
   # La siguiente linea permite redirigir al usuario cuando entra a una pagina no autorizada
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
@@ -24,8 +25,21 @@ class ApplicationController < ActionController::Base
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
-
+  
   def user_not_authorized
     redirect_to(request.referrer || root_path)
+  end
+
+  def store_action
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:user, request.fullpath)
+    end
   end
 end
